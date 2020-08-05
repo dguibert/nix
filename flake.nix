58dbaddc2067e9181c668a64c2e3bf61f3d34141
David Guibert <david.guibert@gmail.com>
2020-08-05 14:17:17 +0200

    binaryTarball: use defaultPkgs

--- flake.nix
+++ flake.nix
@@ -282,7 +282,7 @@
         # the installation script.
         binaryTarball = nixpkgs.lib.genAttrs systems (system:
 
-          with nixpkgsFor.${system};
+          with defaultPkgs;
 
           let
             installerClosureInfo = closureInfo { rootPaths = [ nix cacert ]; };
